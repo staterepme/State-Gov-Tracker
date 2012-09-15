8074e79c33bd9f37febdfa8b7258484ea73abaa7
jwb82 <Jason@jason-blanchards-macbook-pro.local>
2012-09-15 13:31:02 -0400

    Added gitignore file

--- state_gov_tracker/settings.py
+++ state_gov_tracker/settings.py
@@ -106,6 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/Users/Jason/State-Gov-Tracker/state_gov_tracker_app/templates'
 )
 
 INSTALLED_APPS = (
