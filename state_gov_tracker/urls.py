525ef9eb1e645df7ee97e1d510382738c9e7ad3b
jwb82 <Jason@jason-blanchards-macbook-pro.local>
2012-09-15 12:09:56 -0400

    First commit

---
+++ state_gov_tracker/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'state_gov_tracker.views.home', name='home'),
+    # url(r'^state_gov_tracker/', include('state_gov_tracker.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
