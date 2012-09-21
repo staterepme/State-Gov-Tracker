2e84b70a2be70266f3478f66797713e16731c13b
Christopher Brown <chrbrown@sas.upenn.edu>
2012-09-20 21:03:41 -0400

    Added script to merge party information.

--- data_scripts/leg_preferences/calculate_legislator_preferences.R
+++ data_scripts/leg_preferences/calculate_legislator_preferences.R
@@ -1,15 +1,19 @@
 # Calculate Ideal Points of Legislators
-
-# Load Data 
 library("RSQLite")
 library("pscl")
+library("ggplot2")
 
+# Load Data 
 # Path to SQLite Database #
 conn <- dbConnect("SQLite", dbname = "/home/christopher/Dropbox/CongressMonitor/StateGovTracker.db")
 
 # Get Votes Table
 votes <- dbReadTable(conn, "pa_legis_votes")
 vote_sub <- votes[,1:3]
+
+# Get Member Table
+mem_info <- dbReadTable(conn, "officials")
+party_data <- cbind(mem_info$legid, mem_info$party)
 
 # Reshape Votes so that each row is a legislator and column is a vote
 votes_wide <- reshape(vote_sub, idvar="legid", timevar="bill_id", direction="wide")
@@ -31,4 +35,12 @@ id1 <- ideal(rc,
              thin=100,
              verbose=TRUE)  
 
-summary(id1)
+# Combine Ideal Points with Party Data #
+ideal_scores <- cbind(votes_wide[,1], id1$xbar)
+ideal_scores <- data.frame(ideal_scores, row.names=NULL)
+party_df <- data.frame(party_data)
+combined <- merge(party_df, ideal_scores, by.x = "X1", by.y = "V1")
+parties <- combined$X2
+
+qplot(combined$D1, ..density.., data=combined, geom="density", fill="#FF6666", colour = "black", title="Aggregate")
+
