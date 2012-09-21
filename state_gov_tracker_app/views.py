29078fdcf95e1e41faf6bb1512ff3466ec087c08
Christopher Brown <chrbrown@sas.upenn.edu>
2012-09-20 21:01:41 -0400

    Fixed the names of the models being imported from models.py

--- state_gov_tracker_app/views.py
+++ state_gov_tracker_app/views.py
@@ -1,7 +1,7 @@
 # Create your views here.
 from django.http import HttpResponse
 from django.template import Context, loader
-from state_gov_tracker_app.models import State, Party, District, Official
+from state_gov_tracker_app.models import OfficialTweets, Officials
 
 def home(request):
     o = "This is a db query"
