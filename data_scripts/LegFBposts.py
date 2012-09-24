20458dd54dbec80a0bb28cef60b00fffe36f4567
Christopher Brown <chrbrown@sas.upenn.edu>
2012-09-24 17:01:38 -0400

    Began work on auto-updating facebook post data.

---
+++ data_scripts/LegFBposts.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python
+
+from login_credentials import *
+from load_database import *
+import urllib, httplib2, mimetypes,os,sys,re,random,string
+from subprocess import call
+from datetime import datetime
+try:
+	import json
+except ImportError:
+	import simplejson as json
+
+import pprint
+	
+def facebook_token(app_id, app_secret):
+	http = httplib2.Http()
+	content = {}
+	url = 'https://graph.facebook.com/oauth/access_token?client_id='+app_id+'&client_secret='+app_secret+'&grant_type=client_credentials'
+	headers = {'Content-type': 'application/x-www-form-urlencoded'}
+	response, content = http.request(url, 'GET', headers=headers)
+	a = content.rfind('=')
+	return content[a+1:len(content)]
+
+	
+def facebook_news_feed(app_id, app_secret, rep_FB_ID):
+	http = httplib2.Http()
+	content = {}
+	fb_token = facebook_token(app_id, app_secret)
+	url = 'https://graph.facebook.com/' + rep_FB_ID + '/posts?access_token=' + fb_token + '&fields=message'
+	headers = {'Content-type': 'application/x-www-form-urlencoded'}
+	response, content = http.request(url, 'GET', headers=headers)
+	content = json.loads(content)
+	messages = []
+	for entry in content['data']:
+		if 'message' in entry:
+			entry['created_time'].replace
+			messages.append(entry)
+	return messages
+
+def get_facebook_ids():
+	""" Query database and get all facebook IDs, parse for correct IDs"""
+	fb_id_list = []
+	for member in session.query(social_media_ids).all():
+		if member.facebook != "":
+			fb_info = parse_facebook_info(member.facebook)
+			fb_id_list.append((member.legid,fb_info))
+	return fb_id_list
+
+def parse_facebook_info(line):
+	"""Facebook id's right now include more than what we need, this 
+	function parses that line and returns exactly what we need."""
+	if 'pages' in line:
+		search_term = 'pages/'
+	elif 'profile' in line:
+		search_term = 'id='
+	else:
+		search_term = 'facebook.com/'
+	a = line.rfind(search_term)
+	fb_id = line[a+len(search_term):len(line)].rstrip()
+	return fb_id
+
+def download_fb_posts(app_id, app_secret, list_of_fb_ids):
+	"""Downloads most recent facebook posts, returns a list of dictionary entries"""
+	dict_list = []
+	for member_tuple in list_of_fb_ids:
+		posts = facebook_news_feed(app_id, app_secret, member_tuple[1])
+		for post in posts:
+			post[u'legid'] = member_tuple[0]
+			post[u'created_time'] = fix_fb_timestamp(post[u'created_time'])
+			dict_list.append(post)
+	return dict_list
+
+def fix_fb_timestamp(timestamp):
+	new_t = string.replace(timestamp, 'T', ' ')
+	return new_t
+
+def downloaded_posts():
+	"""Returns list of fb post IDs that have 
+	already been downloaded"""
+	fb_post_ids = []
+	for post in session.query(official_posts).all():
+		fb_post_ids.append(post.post_id)
+	return fb_posts_ids
+
+def add_posts_to_db(list_of_dictionary_posts):
+	existing_posts = downloaded_posts()
+	for post in list_of_dictionary_posts:
+		if post['id'] in existing_posts:
+			continue
+		else:
+			new_post = official_posts(legid=post['legid'],
+				post=post['text'],
+				post_id=post['id'],
+				timestamp=post['timestamp'])
+			session.add(new_post)
+			session.commit()
+
+if __name__ == '__main__':
+	pp = pprint.PrettyPrinter(indent=4)
+	mem_list = get_facebook_ids()
+	print mem_list[:2]
+	new_posts = download_fb_posts(fb_app_id, fb_app_secret, mem_list[:2])
+	add_posts_to_db(new_posts)
