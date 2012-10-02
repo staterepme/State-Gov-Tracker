600e75f7bcde84c55e386c6e31f0483cdb109fa1
Christopher Brown <chrbrown@sas.upenn.edu>
2012-10-02 18:27:26 -0400

    Added initial support to add vote information to database with function to access Sunlight API for bill info.

--- data_scripts/gather_recent_votes.py
+++ data_scripts/gather_recent_votes.py
@@ -12,9 +12,17 @@ from load_database import *
 pp = pprint.PrettyPrinter(indent=4)
 
 def get_downloaded_bills():
+	"""Returns list of bill_ids that have already been downloaded"""
 	bill_ids = []
 	for bill in session.query(pa_bills).all():
 		bill_ids.append(bill.bill_id)
+	return bill_ids
+
+def get_downloaded_votes():
+	"""Returns list of vote_ids that have already been downloaded"""
+	vote_ids = []
+	for vote in session.query(pa_votes).all():
+		vote_ids.append(vote.vote_id)
 	return bill_ids
 
 def get_recent_bills():
@@ -28,15 +36,20 @@ def get_bill_detail(bill_id_to_lookup):
 def add_bills_to_db():
 	dl_bills = get_downloaded_bills()
 	bills = get_recent_bills()
-	print "Total of %s bills to gather" %(len(bills))
 	counter = 0
-	for bill in bills:
+	for bill in bills:		
 		if bill['bill_id'] in dl_bills:
 			continue
+		counter += 1
 		new_bill = pa_bills(state="pa", session=bill['session'], title=bill['title'], type=bill['type'][0], chamber=bill['chamber'], created_at=bill['created_at'], updated_at=bill['updated_at'], bill_id=bill['bill_id'])
 		session.add(new_bill)
 	session.commit()
+	print "Added a total of %s bills to database" %(counter)
+
+def add_vote_info_to_db(bill_dict):
+	pass
 
 if __name__ == '__main__':
 	# get_downloaded_bills()
-	add_bills_to_db()
+	# add_bills_to_db()
+	pp.pprint(get_bill_detail('HB 1026'))
