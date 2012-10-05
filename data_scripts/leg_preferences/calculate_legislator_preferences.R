7193f51c822db9c3070d281eb543e1251df4a300
Christopher Brown <chrbrown@sas.upenn.edu>
2012-10-05 11:44:15 -0400

    Updated ideology scores with more votes, separated Reps from Senators.

--- data_scripts/leg_preferences/calculate_legislator_preferences.R
+++ data_scripts/leg_preferences/calculate_legislator_preferences.R
@@ -13,7 +13,13 @@ vote_sub <- votes[,1:3]
 
 # Get Member Table
 mem_info <- dbReadTable(conn, "officials")
-mem_data <- data.frame(cbind(mem_info$legid, mem_info$party, mem_info$chamber))
+mem_data <- data.frame(cbind(mem_info$legid, mem_info$party, mem_info$chamber), stringsAsFactors = FALSE)
+
+lower_mem <- subset(mem_data, X3 %in% c("lower"))
+upper_mem <- subset(mem_data, X3 %in% c("upper"))
+
+lower_legid <- lower_mem[1]
+upper_legid <- upper_mem[1]
 
 # Reshape Votes so that each row is a legislator and column is a vote
 votes_wide <- reshape(vote_sub, idvar="legid", timevar="bill_id", direction="wide")
@@ -21,19 +27,41 @@ votes_wide <- reshape(vote_sub, idvar="legid", timevar="bill_id", direction="wid
 # Create Roll Call Vote Object #
 colnames(votes_wide)[1] <- "legis.names"
 length(colnames(votes_wide))
-vote_wide_sub <- votes_wide[,2:699]
-rc <- rollcall(vote_wide_sub, legis.names=votes_wide[,1])
+
+# Separate out Upper and Lower Chambers #
+upper_wide <- subset(votes_wide, legis.names %in% c(upper_legid$X1))
+lower_wide <- subset(votes_wide, legis.names %in% c(lower_legid$X1))
+upper_vote_wide_sub <- upper_wide[,2:1378]
+lower_vote_wide_sub <- lower_wide[,2:1378]
+
+# Create Roll Call Objects (drop legislators with less than 5 votes and drop votes with less than 5 in opposition)
+rc_upper <- rollcall(upper_vote_wide_sub, missing=c(NA, 99), legis.names=upper_wide[,1])
+rc_lower <- rollcall(lower_vote_wide_sub, missing=c(NA, 99), legis.names=lower_wide[,1])
+
+rc_upper_fixed <- dropRollCall(rc_upper, dropList=list(lop=5, legisMin=5))
+rc_lower_fixed <- dropRollCall(rc_lower, dropList=list(lop=5, legisMin=5))
+summary(rc_upper_fixed)
 
 # Calculate Ideal Points #
-id1 <- ideal(rc,
+id1 <- ideal(rc_upper_fixed,
              d=1,
              startvals="eigen",
              normalize=TRUE,
              store.item=TRUE,
-             maxiter=10000,
-             burnin=1000,
+             maxiter=100000,
+             burnin=10000,
              thin=100,
              verbose=TRUE)  
+
+id2 <- ideal(rc_lower_fixed,
+             d=1,
+             startvals="eigen",
+             normalize=TRUE,
+             store.item=TRUE,
+             maxiter=100000,
+             burnin=10000,
+             thin=100,
+             verbose=TRUE)
 
 # Combine Ideal Points with Party Data #
 ideal_scores <- cbind(votes_wide[,1], id1$xbar)
