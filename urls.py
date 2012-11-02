1c81b29cf6ccb67ec3c8c4a472b5e487bde5518b
Christopher Brown <chrbrown@sas.upenn.edu>
2012-11-02 15:24:12 -0400

    Moved files to correct hierarchy in folder - deleted the --state_gov_tracker-- folder

---
+++ urls.py
@@ -0,0 +1,44 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.conf.urls.defaults import *
+
+#Uncomment these when you're ready to integrate stuff from Chris's project
+#in chris's these were all from StateGovTracker_Django.views import *
+#from state_gov_tracker_app.views import home
+from state_gov_tracker_app.views import MyRep
+from state_gov_tracker_app.views import search_form
+from state_gov_tracker_app.views import search
+#from state_gov_tracker_app.views import search_results
+from state_gov_tracker_app.views import WhichRep, blog_page
+from state_gov_tracker_app.views import profile, pa_tweets, about_myrep
+
+urlpatterns = patterns('',
+	('^$', search_form),
+    ('^results$', WhichRep),
+    ('^MyRep$', MyRep),
+    ('^profile/(.*)$', profile),
+    ('^pa-tweets$', pa_tweets),
+    ('^blog$', blog_page),
+    ('^about$', about_myrep),
+)
+'''
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    url(r'^current_time/', 'state_gov_tracker_app.views.current_datetime'),
+    url(r'^twitter_test/', 'state_gov_tracker_app.views.twitter_view'),
+    url(r'^$', 'state_gov_tracker_app.views.home', name='home'),
+    # url(r'^state_gov_tracker/', include('state_gov_tracker.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+)
+'''
