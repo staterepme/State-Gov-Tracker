0c132d6d89f28493ca7d3024333d12cd24a0aef5
Christopher Brown <chrbrown@sas.upenn.edu>
2012-12-05 18:23:53 -0500

    Added filter to return content type.
    Necessary to pass to javascript function.

--- state_gov_tracker_app/templatetags/profile_filters.py
+++ state_gov_tracker_app/templatetags/profile_filters.py
@@ -1,6 +1,7 @@
 from django import template
 
 register = template.Library()
+from django.contrib.contenttypes.models import ContentType
 
 @register.filter
 def shorten_date(value):
@@ -15,3 +16,15 @@ def num_to_vote(vote):
     vote_string = "%s" %(vote)
     vote_dictionary = {'0': 'Nay', '1': 'Yea', '99': 'Other'}
     return vote_dictionary[vote_string]
+
+@register.filter
+def vote_total(object):
+    return object.vote_total
+
+@register.filter
+def content_type(object):
+    return "%s.%s" % (object._meta.app_label, object._meta.object_name)
+
+@register.filter
+def ct(object):
+    return "%s%s" % (object._meta.app_label, object._meta.object_name)
