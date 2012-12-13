04694577cf970fb67d2b15dbb73b1b922090cd81
Christopher Brown <chrbrown@sas.upenn.edu>
2012-12-13 08:47:35 -0500

    Added URLs for APIs

--- urls.py
+++ urls.py
@@ -6,7 +6,6 @@ from django.conf.urls.defaults import *
 #in chris's these were all from StateGovTracker_Django.views import *
 #from state_gov_tracker_app.views import home
 from state_gov_tracker_app.views import search_form
-from state_gov_tracker_app.views import search
 #from state_gov_tracker_app.views import search_results
 from state_gov_tracker_app.views import WhichRep
 # from state_gov_tracker_app.views import RecordVote
@@ -14,6 +13,14 @@ from state_gov_tracker_app.views import profile, pa_tweets, about_myrep
 from blog.views import Blog, Article
 from secretballot.views import vote
 from django.contrib import admin
+from state_gov_tracker_app.api import PreferencesResource, PR_Resource
+from tastypie.api import Api
+
+
+v1_api = Api(api_name='v1')
+v1_api.register(PreferencesResource())
+v1_api.register(PR_Resource())
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -29,7 +36,7 @@ urlpatterns = patterns('',
     (r'^accounts/', include('registration.backends.default.urls')),
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-
+    (r'^api/', include(v1_api.urls)),
     url(r'^vote/$', vote), 
 )
 
