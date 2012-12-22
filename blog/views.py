8617222b319cb37d246dadcdfe4b64a5261dfdbd
Christopher Brown <chrbrown@sas.upenn.edu>
2012-12-22 17:08:11 -0500

    Added view for previewing blog

--- blog/views.py
+++ blog/views.py
@@ -4,13 +4,14 @@ from django.shortcuts import render_to_response
 from models import *
 from django.core.paginator import Paginator, InvalidPage, EmptyPage
 from django.template import RequestContext
+from django.contrib.admin.views.decorators import staff_member_required
 
 
 def Blog(request):
     """Main listing."""
-    posts = Post.objects.all().order_by("-created")
+    posts = Post.objects.filter(publish=1).all().order_by("-created")
     paginator = Paginator(posts, 2)
-    recent_posts = Post.objects.all().order_by("-created")[:4]
+    recent_posts = Post.objects.filter(publish=1).all().order_by("-created")[:4]
     links = LinksToRead.objects.all()
 
     try:
@@ -26,12 +27,27 @@ def Blog(request):
     return render_to_response("blog.html", {"posts": posts, "links": links, "recentposts": recent_posts})
 
 
+@staff_member_required
+def BlogPreview(request):
+    posts = Post.objects.filter(publish=0).all().order_by("-created")
+    recent_posts = Post.objects.filter(publish=0).all().order_by("-created")[:4]
+    links = LinksToRead.objects.all()
+
+    return render_to_response("blog.html", {"posts": posts, "links": links, "recentposts": recent_posts})
+
+
 def Article(request, post_num):
     """Page for Post"""
-    post = Post.objects.get(pk=post_num)
-    posts = Post.objects.all().order_by("-created")
+    try:
+        post = Post.objects.filter(publish=1).get(pk=post_num)
+    except:
+        post = None
+        recent_posts = Post.objects.filter(publish=1).all().order_by("-created")[:4]
+        links = LinksToRead.objects.all()
+        render_to_response("blog_post.html", {"links": links, "recentposts": recent_posts}, context_instance=RequestContext(request))
+    posts = Post.objects.filter(publish=1).all().order_by("-created")
     paginator = Paginator(posts, 2)
-    recent_posts = Post.objects.all().order_by("-created")[:4]
+    recent_posts = Post.objects.filter(publish=1).all().order_by("-created")[:4]
     links = LinksToRead.objects.all()
 
     try:
