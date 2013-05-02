254c5272e2ef074ada93867d438da19be7594710
Chris Brown <chrbrown@sas.upenn.edu>
2013-05-02 10:12:41 -0400

    Create CSVs to use as templates for filling in twitter/facebook/press release data

--- state_gov_tracker_app/management/commands/csv_export.py
+++ state_gov_tracker_app/management/commands/csv_export.py
@@ -1,9 +1,10 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
+from django.forms.models import model_to_dict
 
 from state_gov_tracker_app.models import Officials
 
-import csv
+import os
 
 class Command(BaseCommand):
     args = '<list of data/models to export csvs for>'
@@ -12,7 +13,7 @@ class Command(BaseCommand):
         super(Command, self).__init__(*args, **kwargs)
         self.twitter = {
             'path':'raw_data/twitter/{0}.csv'.format(settings.STATE_FILTER),
-            'fields':['legid', 'fullname', 'twitter']
+            'fields':['legid', 'fullname', 'party', 'chamber', 'twitter']
         }
         self.press_release = {
             'path':'raw_data/pr_configs/{0}.csv'.format(settings.STATE_FILTER),
@@ -26,14 +27,22 @@ class Command(BaseCommand):
 
         self.data_types = [self.twitter, self.press_release, self.facebook]
 
-    def printrow(dictionary, fields):
+    def printrow(self, dictionary, fields):
         s = str(dictionary[fields[0]])
         for field in fields[1:]:
             s = s + ',' + dictionary[field]
         return s
-        
+
+    def check_path(self, path_to_file):
+        """Check if path exists, if no, create path"""
+        try:
+            os.makedirs(os.path.split(path_to_file)[0])
+        except OSError, e:
+            pass
+    
     def write_csv(self):
         for i in self.data_types:
+            self.check_path(i['path'])
             f = open(i['path'], 'w')
             data = Officials.objects.filter(active='True')
             headerrow = i['fields'][0]
@@ -41,8 +50,10 @@ class Command(BaseCommand):
                 headerrow = headerrow + ',' + field
             f.write(headerrow+'\n')
             for d in data:
-                s = "{0}, {1}, {2}".format()
-            f.write(s+'\n')
+                d_dict = model_to_dict(d)
+                s = self.printrow(d_dict, i['fields'])
+                f.write('{0}\n'.format(s))
+            f.close()
 
     def handle(self, *args, **options):
         self.write_csv()
