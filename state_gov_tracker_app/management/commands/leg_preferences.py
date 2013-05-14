36f43776045035371964ff95e34f16f6fb721b21
Chris Brown <chrbrown@sas.upenn.edu>
2013-05-14 18:13:23 -0400

    Delete CSV file of preferences if it exists before creating a new one

--- state_gov_tracker_app/management/commands/leg_preferences.py
+++ state_gov_tracker_app/management/commands/leg_preferences.py
@@ -2,7 +2,7 @@ from django.core.management.base import BaseCommand, CommandError
 
 from django.conf import settings
 
-import os, sys
+import os, sys, subprocess
 
 from state_gov_tracker_app.models import Preferences
 
@@ -13,4 +13,15 @@ class Command(BaseCommand):
 
     def delete_csv(self):
         """Deletes preference csv file if it exists"""
-        
+        print 'Deleting current preferences file'
+        try:
+            os.remove('current_preferences.csv')
+        except OSError:
+            print 'No Current Preferences to Delete'
+
+    def calculate_preferences(self):
+        """Runs R code to calculate legislator preferences"""
+        subprocess.call("R CMD BATCH calculate_legislator_preferences.R")
+
+    def handle(self, *args, **options):
+        self.delete_csv()
